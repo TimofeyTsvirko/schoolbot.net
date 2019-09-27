44c19f31642851c290c6afee08961c78b47742ee
TimofeyTsvirko <TimofeyTsvirko@users.noreply.github.com>
2019-09-27 20:23:51 +0300

    Update submit.php

--- submit.php
+++ submit.php
@@ -24,30 +24,11 @@ if($_POST['type']=='searchlessons'){
 
 	
 	} else {
-		$query = "SELECT * FROM schedule where grade='$grade' and day='$day'";
+		$query = "SELECT * FROM schedule where grade='$grade' and profile='$profile' and day='$day' and city='Кемь' and school='МБОУСОШ1'";
 		$result = @mysqli_query($dbc,$query);
 
-		if($result){
-			$i=0;
-			while($row = mysqli_fetch_array($result)){
-				$i++;
-			}
-			if($i==0){
-				echo '<div class="heading">
-						<span>Уроки</span>
-						<span>Кабинет</span>
-					</div>
-					<ol>';
 
-				for ($k=0; $k < 7; $k++) {
-					echo "<li><input list='lessons' id='lesson-$k'> <input type='text' id='class-$k'></li>";
-				}
-
-
-				echo '<datalist id="lessons">';
-				$datalist_lessons_query = 'SELECT lessons FROM schedule';
-				$datalist_lessons_result = @mysqli_query($dbc,$query);
-				$lessons_set = array('Русский', 
+		$lessons_set = array('Русский', 
 									'Математика', 
 									'Литература', 
 									'Физкультура', 
@@ -63,6 +44,26 @@ if($_POST['type']=='searchlessons'){
 									'Черчение', 
 									'Обществознание ',
 									'Иностранный язык ');
+		if($result){
+			$i=0;
+			$schedule_data = array();
+			while($row = mysqli_fetch_array($result)){
+				$schedule_data[]=$row;
+				$i++;
+			}
+			if($i==0){
+				echo '<div class="heading">
+						<span>Уроки</span>
+						<span>Кабинет</span>
+					</div>
+					<ol>';
+
+				for ($k=0; $k < 7; $k++) {
+					echo "<li><input list='lessons' id='lesson-$k'> <input type='text' id='class-$k'></li>";
+				}
+
+
+				echo '<datalist id="lessons">';
 				foreach ($lessons_set as $lesson) {
 					echo "<option value='$lesson'></option>";
 				}
@@ -82,6 +83,41 @@ if($_POST['type']=='searchlessons'){
 			</select>
 			<br>
 			<input type="button" value="Сохранить" onclick="SubmitUpdateLessons();">';
+
+		}elseif ($i==1) {
+			echo '<div class="heading">
+						<span>Уроки</span>
+						<span>Кабинет</span>
+					</div>
+					<ol>';
+			$lessons_classes = explode(';', $schedule_data[0][lessons]);
+			$k = 0;
+			foreach ($lessons_classes as $pair) {
+				$pair = explode('_', $pair);
+				echo "<li><input list='lessons' id='lesson-$k' value=$pair[0]> <input type='text' id='class-$k' value=$pair[1]></li>";
+				$k++;
+			}
+			echo '<datalist id="lessons">';
+				foreach ($lessons_set as $lesson) {
+					echo "<option value='$lesson'></option>";
+				}
+			echo '</datalist>
+					</ol>
+					<label>Срок:</label>
+					<select id="priority">
+						<option value="main">Основной</option>
+						<option value="1">1 раз</option>
+						<option value="2">2 раза</option>
+						<option value="3">3 раза</option>
+						<option value="4">4 раза</option>
+						<option value="5">5 раз</option>
+						<option value="6">6 раз</option>
+						<option value="7">7 раз</option>
+			</select>
+			<br>
+			<input type="button" value="Сохранить" onclick="SubmitUpdateLessons();">';
+
+		}else{
 
 		}
 	}else{
@@ -116,8 +152,9 @@ if($_POST['type']=='updatelessons'){
 		$day = $_POST['day'];
 		$lessons = $_POST['lessons_string'];
 		$priority = $_POST['priority'];
+		$time_created = time();
 
-		$query = "SELECT * FROM schedule where grade='$grade' and day='$day'";
+		$query = "SELECT * FROM schedule where grade='$grade' and profile='$profile' and day='$day'";
 		$result = @mysqli_query($dbc,$query);
 
 		if($result){
@@ -126,7 +163,7 @@ if($_POST['type']=='updatelessons'){
 				$i++;
 			}
 			if($i==0){
-				$query="INSERT INTO schedule (city,school,grade,profile,day,lessons,priority) VALUES ('Кемь','МБОУСОШ1','$grade','$profile','$day', '$lessons','$priority')";
+				$query="INSERT INTO schedule (city,school,grade,profile,day,lessons,priority,time_created) VALUES ('Кемь','МБОУСОШ1','$grade','$profile','$day', '$lessons','$priority',$time_created)";
 				$result = @mysqli_query($dbc,$query);
 				if($result){
 					echo 'Успешно!';
@@ -134,6 +171,10 @@ if($_POST['type']=='updatelessons'){
 					echo 'Ошибка:';
 					echo mysqli_error($dbc);
 				}
+			}elseif ($priority=='main') {
+				// 
+				// $query="UPDATE schedule SET lessons=$lessons, "
+				// 
 			}
 		}else{
 			echo 'Could not issue database query';
