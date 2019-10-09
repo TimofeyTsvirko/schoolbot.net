c7076af407bb02b4f59327c252ddd6948a206a74
TimofeyTsvirko <TimofeyTsvirko@users.noreply.github.com>
2019-10-09 20:19:13 +0300

    Create bot_homepage.php

---
+++ bot/bot_homepage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script src="../submit.js"></script>
+	<script src="../jquery.js"></script>
+	<meta name="viewport" content="widht=device-width,initial-scale=1">
+</head>
+<body>
+	<form>
+		<input type="text" id="text">
+		<input type="button" value="Submit" onclick="SubmitBotText();">
+	</form>
+	<div class="result"></div>
+</body>
+</html>
